dd18985bdb29bac887e449ae83f3413ac32ebe2c
Cody Snell <codyjsnell@yahoo.com>
2020-12-10 19:35:21 -0600

    changes header link

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -6,13 +6,13 @@ export default function Header(props){
     return(
         <header className="Header">
             <Link to = '/'>
-            <h1>Project 3</h1>
+            <h1>Book-It</h1>
             </Link>
             <nav>
                 <ul className="NavLinks">
                     
             <li>{props.user && <Link to='' onClick={props.handleLogout}>Log Out</Link>}</li>
-            <li>{props.user && <Link to= '/dashboard'>Dashboard</Link>}</li>                   
+            <li>{props.user && <Link to= '/search'>Search</Link>}</li>                   
             <li>{!props.user && <Link to= '/signup'>Signup</Link>}</li>
             <li>{!props.user && <Link to ='/login'>Login</Link>}</li>                        
                  
