b3a5e4a28e6411821503733340423b455ad81259
Cody Snell <codyjsnell@yahoo.com>
2020-12-15 08:45:49 -0600

    returns 40 results

--- src/services/bookService.js
+++ src/services/bookService.js
@@ -4,7 +4,7 @@ const API_URL='https://www.googleapis.com/books/v1/volumes?q='
 const GOOGLEAPIKEY=process.env.REACT_APP_GOOGLEAPIKEY
 // google book search
 export function getBooks(i){
-    return fetch(API_URL+`${i}${process.env.REACT_APP_GOOGLEAPIKEY}`).then(res=>res.json())
+    return fetch(API_URL+`${i}&maxResults=40`+GOOGLEAPIKEY).then(res=>res.json())
 }
 
 // NYT
@@ -14,5 +14,5 @@ const NYTAPIKEY= process.env.REACT_APP_NYTAPIKEY
 // home page top books
 
 export function getNytBooks(){
-    return fetch(HOME_API+`${process.env.REACT_APP_NYTAPIKEY}`).then(res=>res.json())
+    return fetch(HOME_API+NYTAPIKEY).then(res=>res.json())
 }
